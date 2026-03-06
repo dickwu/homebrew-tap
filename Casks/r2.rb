d4c2a67e601186274660ebc174788a92573f9678
farmerwu <davidwu192@gmail.com>
2026-03-06 13:16:01 -0600

    feat: add r2 cask v0.1.64

---
+++ Casks/r2.rb
@@ -0,0 +1,27 @@
+cask "r2" do
+  version "0.1.64"
+
+  on_arm do
+    url "https://github.com/dickwu/r2/releases/download/v#{version}/r2_#{version}_aarch64.dmg"
+    sha256 "33aabea53e9f8573c708913ddc541ac8a00d97b42ef45e806a73d5b360b4ef0a"
+  end
+
+  on_intel do
+    url "https://github.com/dickwu/r2/releases/download/v#{version}/r2_#{version}_x64.dmg"
+    sha256 "4bf5988e8c0a166584b414ebacd07da15ab40771308abc5b272bb21740fbe0c0"
+  end
+
+  name "R2 Client"
+  desc "Desktop client for managing S3-compatible storage (Cloudflare R2, AWS S3, MinIO, RustFS)"
+  homepage "https://github.com/dickwu/r2"
+  license "MIT"
+
+  app "r2.app"
+
+  zap trash: [
+    "~/Library/Application Support/com.lifefarmer.r2",
+    "~/Library/Caches/com.lifefarmer.r2",
+    "~/Library/Preferences/com.lifefarmer.r2.plist",
+    "~/Library/Saved Application State/com.lifefarmer.r2.savedState",
+  ]
+end
