08b77e6b0778069c2157ff376b3fcc9f31e63f72
farmerwu <davidwu192@gmail.com>
2026-03-25 13:50:50 +0000

    chore: update ironguard to 0.2.1

--- Formula/ironguard.rb
+++ Formula/ironguard.rb
@@ -6,24 +6,24 @@ class Ironguard < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/dickwu/ironguard/releases/download/v0.2.1/ironguard-macos-aarch64.tar.gz"
+      url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-aarch64.tar.gz"
       sha256 "69fbef5d18ca61a95e8341d091cd2456524313d1bc7b3ef078d610d2da01c60c"
     end
 
     on_intel do
-      url "https://github.com/dickwu/ironguard/releases/download/v0.2.1/ironguard-macos-x86_64.tar.gz"
+      url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-x86_64.tar.gz"
       sha256 "15f0e3125649f7b53831c24555797bd30ddd02b9b0b1ab53e8420fe988344c51"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dickwu/ironguard/releases/download/v0.2.1/ironguard-linux-aarch64.tar.gz"
+      url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-aarch64.tar.gz"
       sha256 "bba1021fa7e07dc4d278cdb482b0bfb0a07a4e0173babeea0d6981820f0f5383"
     end
 
     on_intel do
-      url "https://github.com/dickwu/ironguard/releases/download/v0.2.1/ironguard-linux-x86_64.tar.gz"
+      url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-x86_64.tar.gz"
       sha256 "504abda541d3e2f87d90047e5ab15e6db04371b001790188543d14eb8e26f99b"
     end
   end
