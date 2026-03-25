2d07a00067ae671bb0ac28e600d22bcb66767e1f
farmerwu <davidwu192@gmail.com>
2026-03-25 18:04:36 +0000

    chore: update ironguard to 0.2.2

--- Formula/ironguard.rb
+++ Formula/ironguard.rb
@@ -1,35 +1,36 @@
 class Ironguard < Formula
   desc "Modern cross-platform WireGuard implementation in Rust"
   homepage "https://github.com/dickwu/ironguard"
-  version "0.2.1"
+  version "0.2.2"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-aarch64.tar.gz"
-      sha256 "69fbef5d18ca61a95e8341d091cd2456524313d1bc7b3ef078d610d2da01c60c"
+      sha256 "b1b322f0c80ef56c078bb9af65f401a456fa90f387f2b2a43cc222f0f7abdb5e"
     end
 
     on_intel do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-x86_64.tar.gz"
-      sha256 "15f0e3125649f7b53831c24555797bd30ddd02b9b0b1ab53e8420fe988344c51"
+      sha256 "f846da5d1a1748b9106490d0c819258919c9f11a9c999e6e7f326ccc6fa3e769"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-aarch64.tar.gz"
-      sha256 "bba1021fa7e07dc4d278cdb482b0bfb0a07a4e0173babeea0d6981820f0f5383"
+      sha256 "632530a902c2eec4575db24099e943c54c036dd074ede280b589e193e3677e75"
     end
 
     on_intel do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-x86_64.tar.gz"
-      sha256 "504abda541d3e2f87d90047e5ab15e6db04371b001790188543d14eb8e26f99b"
+      sha256 "29a7d89ebb9dbc987a0f16c12fafc8cdd1c00bf8b5afd3f7da8e9d8e20d46c0e"
     end
   end
 
   def install
     bin.install "ironguard"
+    bin.install "ironguard-tui"
   end
 
   test do
