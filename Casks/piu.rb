7de0849f5df262fb63d9a195c59c7bf3043690e9
farmerwu <davidwu192@gmail.com>
2026-03-25 15:03:59 +0000

    chore: bump PIU cask to v0.1.41

--- Casks/piu.rb
+++ Casks/piu.rb
@@ -1,13 +1,13 @@
 cask "piu" do
-  version "0.1.40"
+  version "0.1.41"
 
   on_arm do
     url "https://github.com/dickwu/piu/releases/download/v#{version}/Piu_#{version}_aarch64.dmg"
-    sha256 "2f9c53097c6f0adbc6359e97d8d7affa9c263cb3903603760bb6adeaf81487f0"
+    sha256 "7a61ec5a540372bea8cb277534e2be67ae100c50ac351a4ed1367912aaee1c35"
   end
   on_intel do
     url "https://github.com/dickwu/piu/releases/download/v#{version}/Piu_#{version}_x64.dmg"
-    sha256 "572db1a59deb304edb2e3328692bffaad9fdb3ae02ed3f010ead20073370876c"
+    sha256 "e7731afc2270848e115945a4772f577fc4cf2444cb92ff16697d424f2dbb22f4"
   end
 
   name "PIU"
