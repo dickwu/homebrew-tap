a164e1811597d3993888aeaaf078e7af9310744a
farmerwu <davidwu192@gmail.com>
2026-03-26 16:53:44 +0000

    chore: update ironguard to 0.3.0

--- Formula/ironguard.rb
+++ Formula/ironguard.rb
@@ -1,30 +1,30 @@
 class Ironguard < Formula
   desc "Modern cross-platform WireGuard implementation in Rust"
   homepage "https://github.com/dickwu/ironguard"
-  version "0.2.4"
+  version "0.3.0"
   license "GPL-3.0-or-later"
 
   on_macos do
     on_arm do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-aarch64.tar.gz"
-      sha256 "c20f2bbdfc12ebb4f847a000c57930e20e116ce51b541c991c1c931846c48b04"
+      sha256 "404796992c5bde796ffc567cb7c965b28b7575d2452669a372c005c6140de13c"
     end
 
     on_intel do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-macos-x86_64.tar.gz"
-      sha256 "b4222c199f39eda7e6cc7b364bedd678940f403ad09cd96b240f517ed6110c57"
+      sha256 "b255b3aec957d9135f14199ce056d3afa467fa6dae98f2099f34f6973e28c701"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-aarch64.tar.gz"
-      sha256 "35d986898bb766fc08828e9bfebb8219068c5e5d068cf5afb0a089a777757939"
+      sha256 "8e70d35fca89f79b325c92b80212f9feb127fa02c46968fcf620f5c147ad277d"
     end
 
     on_intel do
       url "https://github.com/dickwu/ironguard/releases/download/v#{version}/ironguard-linux-x86_64.tar.gz"
-      sha256 "748815ad7671e319c9a1575469cbdee80ef3510fc6c2e00f4078dd8fd3b291bc"
+      sha256 "7195464e4d6b3ea841ec7796cb7c7d72a3a79a5e1859b72b11fff3859b0a9fb3"
     end
   end
 
