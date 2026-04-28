d71c18e4f3ad1ad9b648cbf0c76dcc5e6c835356
farmerwu <davidwu192@gmail.com>
2026-04-28 04:39:01 +0000

    chore: update wordbrain cask to 0.1.11

--- Casks/wordbrain.rb
+++ Casks/wordbrain.rb
@@ -1,6 +1,6 @@
 cask "wordbrain" do
-  version "0.1.10"
-  sha256 "3d2f847e80e55299c33648175b305c9a1f9d1d64a12793bbfa9b1d060a8b1757"
+  version "0.1.11"
+  sha256 "b6e0cef7c10cce952090d491398334f8fe8450af1e165d4a12a46d9a95cb396d"
 
   url "https://github.com/dickwu/wordbrain/releases/download/v#{version}/WordBrain_#{version}_universal.dmg"
   name "WordBrain"
