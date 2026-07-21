09118f90cb500bb63b25fb9b89ac61de5b7a6604
farmerwu <davidwu192@gmail.com>
2026-07-21 15:11:38 +0000

    chore: update r2 cask to 0.2.23

--- Casks/r2.rb
+++ Casks/r2.rb
@@ -1,14 +1,14 @@
 cask "r2" do
-  version "0.2.22"
+  version "0.2.23"
 
   on_arm do
     url "https://github.com/dickwu/r2/releases/download/v#{version}/r2_#{version}_aarch64.dmg"
-    sha256 "255246bc55272fc4cff27bf2b3fbae83a48c1a8fc145aa4348780b635f8b6a7a"
+    sha256 "5b7be4b59a7355cf242985e51c25b20c0c32b07e325d5baa10720b635b9cceda"
   end
 
   on_intel do
     url "https://github.com/dickwu/r2/releases/download/v#{version}/r2_#{version}_x64.dmg"
-    sha256 "c650ed714112d7fb59169aaaf558fd1797467d74ab96498fe2cf40edc3969545"
+    sha256 "ea35b3540129c28b7dd1425abaff9ee00158d530ebae642100a04a37510bf417"
   end
 
   name "R2 Client"
